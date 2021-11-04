1a2e5eb5850e92cd56c5e792d14923de3681ac44
AshaniImalsha <ashaniimalsha26@gmail.com>
2021-11-05 02:03:22 +0530

    update

--- controllers/Systemuser.php
+++ controllers/Systemuser.php
@@ -17,4 +17,58 @@ class Systemuser extends controller{
         
     }
 
+    function showpage_myprofile(){
+
+        $this->view->users = $this->model->getData($_SESSION['login_user']);
+        $this->view->render('myprofile');
+    }
+
+    function saveform(){
+
+        $pass = $_POST['pass'];
+        $cpass = $_POST['cpass'];
+        $npass = $_POST['npass'];
+
+        if($pass == $_SESSION['password']){
+
+            $hash = password_hash($npass, PASSWORD_DEFAULT);
+
+            if($cpass == $npass){
+                $pass = $this->model->setNewPassword($_SESSION['login_user'],$hash); 
+                if($pass == 1)
+                    $_SESSION['pass']="yes";
+                else
+                    $_SESSION['pass']="no";
+            }
+            else{
+                $_SESSION['pass']="no";
+            }
+        }else{
+            $_SESSION['pass']="no";
+        }
+
+        $this->showpage_myprofile();
+
+    }
+
+    function savephoto(){
+
+        $image_name = $_FILES['image']['name'];
+        $tempname = $_FILES['image']['tmp_name'];
+        $folder = "../FINAL/Asserts/".$image_name;
+
+        $this->model->setDataPhoto($_SESSION['login_user'],$image_name);
+
+        if (move_uploaded_file($tempname, $folder))  {
+            $_SESSION['photo']="yes";
+            
+        }else{
+            $_SESSION['photo']="no";
+            
+        }
+
+        $this->showpage_myprofile();
+        
+    }
+
 }
