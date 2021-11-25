3583c3f82366a9b85b3e25fb3d39ce8a7a043c9f
Sudam Munasinghe <85274135+Sudam1998@users.noreply.github.com>
2021-11-25 17:30:45 +0530

    update member controller

--- controllers/Member.php
+++ controllers/Member.php
@@ -7,5 +7,28 @@ class Member extends controller{
         parent::__construct();
         session_start();
     }
+    function showpage_myprogressAccepted(){
+        $this->view->users =  $this->model->getAcceptedData();
+        $this->view->render('myprogressAccepted');
+    }
+    function showpage_myprogressCompleted(){
+         $this->view->users =  $this->model->getCompletedData();
+         $this->view->render('myprogressCompleted');
+
+       
+    }
+    function showpage_myprogressOverdue(){
+       $this->view->users =  $this->model->getOverdueData();
+       $this->view->render('myprogressOverdue');
+    }
+    function showpage_myprogressPending(){
+       $this->view->users =  $this->model->getPendingData();
+       $this->view->render('myprogressPending');
+    }
+    function showpage_myprogressInprogressSelect(){
+       $this->view->users =  $this->model->getInprogressSelectData();
+       $this->view->render('myprogressInprogressSelect');
+
+    }
     
 }
