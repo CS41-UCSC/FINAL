64aae9af01ee9a603560107069d87bb2ed4c3d43
AshaniImalsha <ashaniimalsha26@gmail.com>
2021-11-27 20:41:51 +0530

    get team members

--- models/Team_Model.php
+++ models/Team_Model.php
@@ -21,4 +21,20 @@ class Team_Model extends Model{
         return $this->db->runQuery($sql);
     }
 
+    function getTeamMembers($tid){
+
+        $mem = "SELECT team_member.EmpID FROM team_member WHERE TeamID = '$tid' ";
+        $led = "SELECT team_leader.EmpID FROM team_leader WHERE TeamID = '$tid' ";
+
+        $sql1 = "SELECT EmpID, EmpName FROM systemuser WHERE systemuser.EmpID = ANY ($mem) " ;
+        $sql2 = "SELECT EmpID, EmpName FROM systemuser WHERE systemuser.EmpID = ANY ($led) " ;
+        
+        $res1 = $this->db->runQuery($sql1);
+        $res2 = $this->db->runQuery($sql2);
+
+
+        return array_merge($res1, $res2);
+
+    }
+
 }
