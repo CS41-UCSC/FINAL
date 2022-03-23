486d9987802e29b33ff58ee508429ec77c6a0436
Sudam Munasinghe <85274135+Sudam1998@users.noreply.github.com>
2022-03-23 17:03:37 +0530

    add systemuser_model

--- models/Systemuser_Model.php
+++ models/Systemuser_Model.php
@@ -107,6 +107,88 @@ class Systemuser_Model extends Model{
             return false;
         }
     }
+    function insertSkill($skill,$percentage,$r){
+		
+		// $sql="";
+	
+			$sql = "INSERT INTO star_rate ( `skill_name`,`percent`,`EmpID`) VALUES ('$skill','$percentage','$r')";
+            // $sql = "INSERT INTO star_rate ( `skill_name`) VALUES ('$skill')";
+            $this->db->query($sql);
+            // if (($this->db->query($sql)) == TRUE) {
+            //     return true;
+            // } else {
+            //     return false;
+            // }
+
+            if($percentage == "10%"){
+                $sqlk ="INSERT INTO star_rate (`star_rating` ) VALUES ('$percentage')";
+                $this->db->query($sqlk);
+            }
+            elseif($percentage == "20%"){
+                $sqlk ="INSERT INTO star_rate (`star_rating` ) VALUES ('$percentage')";
+                $this->db->query($sqlk);
+            }
+            elseif($percentage == "30%"){
+                $sqlk ="INSERT INTO star_rate (`star_rating` ) VALUES ('$percentage')";
+                $this->db->query($sqlk);
+            }
+            elseif($percentage == "40%"){
+                $sqlk ="INSERT INTO star_rate (`star_rating` ) VALUES ('$percentage')";
+                $this->db->query($sqlk);
+            }
+            elseif($percentage == "50%"){
+                $sqlk ="INSERT INTO star_rate (`star_rating` ) VALUES ('$percentage')";
+                $this->db->query($sqlk);
+            }
+            elseif($percentage == "60%"){
+                $sqlk ="INSERT INTO star_rate (`star_rating` ) VALUES ('$percentage')";
+                $this->db->query($sqlk);
+            }
+            elseif($percentage == "70%"){
+                $sqlk ="INSERT INTO star_rate (`star_rating` ) VALUES ('$percentage')";
+                $this->db->query($sqlk);
+            }
+            elseif($percentage == "80%"){
+                $sqlk ="INSERT INTO star_rate (`star_rating` ) VALUES ('$percentage')";
+                $this->db->query($sqlk);
+            }
+            elseif($percentage == "90%"){
+                $sqlk ="INSERT INTO star_rate (`star_rating` ) VALUES ('$percentage')";
+                $this->db->query($sqlk);
+            }
+            else{
+                $sqlk ="INSERT INTO star_rate (`star_rating` ) VALUES ('$percentage')";
+                $this->db->query($sqlk);
+            }
+            
+           
+           
+            
+		// if($this->db->query($sql) == true){
+        //         $_SESSION['my-profile'] = "yes";
+        // }else{
+        //         $_SESSION['my-profile'] = "no";
+        // }
+       
+
+    
+        // try{
+        //     $this->db->exec($sql);
+        //         $_SESSION['error-msg'] = "New record created successfully";
+        //     }
+        // catch(PDOException $e)
+        // {
+
+    	//     $_SESSION['error-msg'] = $e->getMessage();
+        // }
+
+    }
+    function getSkillData($temp){
+        $r = $_SESSION['login_user'];
+        $sql1 = "SELECT skill_name,percent FROM star_rate WHERE EmpID in ('$temp')";
+        return $this->db->runQuery($sql1);
+
+    }
 
 }
 
