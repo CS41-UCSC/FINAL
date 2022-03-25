365df471058ad78cf127f715901b1fdc1627e444
mtbstn <thushibstn24@gmail.com>
2022-03-25 15:45:25 +0530

    progressReport controller

--- controllers/progressReport.php
+++ controllers/progressReport.php
@@ -6,13 +6,29 @@ class progressReport extends Controller{
 	{
 		parent::__construct();
 		session_start();
+
+		$date = date_create();
+		$this->current_month = date_format($date, "Y-m");
+
+		$this->annual = 14;
+		$this->casual = 4;
+		$this->sick = 10;
 	}
 	
 	function index(){
 		$this->view->notifications = $this->model->getNotifications();
 		$this->view->notificationCount = $this->model->getNotificationCount();
 
-		
+		$empID = $_GET['ID'];
+		$this->view->userdata = $this->model->getUserData($empID);
+		$this->view->assignedTask = $this->model->getTaskData($empID, $this->current_month, NULL);
+		$this->view->pendingTask = $this->model->getTaskData($empID, $this->current_month, "Pending");
+		$this->view->inprogressTask = $this->model->getTaskData($empID, $this->current_month, "InPRogress");
+		$this->view->completedTask = $this->model->getTaskData($empID, $this->current_month, "Completed");
+		$this->view->approvedLeaves = $this->model->getLeaveData($empID, $this->current_month, NULL);
+		$this->view->remainingLeaves['sick'] = $this->sick - $this->model->getLeaveData($empID, $this->current_month, "Sick")[0][0];
+		$this->view->remainingLeaves['annual'] = $this->annual - $this->model->getLeaveData($empID, $this->current_month, "Annual")[0][0];
+		$this->view->remainingLeaves['casual'] = $this->casual - $this->model->getLeaveData($empID, $this->current_month, "Casual")[0][0];
 
 		$this->view->render('progressReport');
 	}
