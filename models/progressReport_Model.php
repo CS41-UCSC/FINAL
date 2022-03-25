83cd2010c21b92013c388c29507681159d301f23
mtbstn <thushibstn24@gmail.com>
2022-03-25 15:44:07 +0530

    progressReport model

--- models/progressReport_Model.php
+++ models/progressReport_Model.php
@@ -7,12 +7,51 @@ class progressReport_Model extends Model{
         parent::__construct();
     }
 
-	function getTaskData(){
-
+	function getUserData($empID){
+		$sql = "SELECT systemuser.EmpID, systemuser.EmpName FROM systemuser 
+		WHERE systemuser.EmpID = '$empID'";
+		return $this->db->runQuery($sql);
 	}
 
-	function getLeaveData(){
+	function getTaskData($empID, $month, $status){
+
+		if($status == NULL){
+			$sql = "SELECT COUNT(*), SUM(RequiredTime) FROM task_assign 
+			WHERE task_assign.AssignedTo = '$empID' 
+			AND DATE_FORMAT(task_assign.AssignedTime, '%Y-%m')='$month'";
+		}
+		elseif(($status == "Pending") || ($status == "InProgress")){
+			$sql = "SELECT COUNT(*), SUM(RequiredTime) FROM task_assign 
+			WHERE task_assign.AssignedTo = '$empID' 
+			AND DATE_FORMAT(task_assign.AssignedTime, '%Y-%m')<='$month' 
+			AND DATE_FORMAT(task_assign.DueDate, '%Y-%m')>='$month' AND task_assign.TaskStatus = '$status'";
+		}
+		else{
+			$sql = "SELECT COUNT(*), SUM(RequiredTime) FROM task_assign 
+			WHERE task_assign.AssignedTo = '$empID' 
+			AND DATE_FORMAT(task_assign.CompletedDate, '%Y-%m')='$month'";
+		}
 		
+		return $this->db->runQuery($sql);
+	}
+
+	function getLeaveData($empID, $month, $type){
+		$status = "Approved";
+		if($type == NULL){
+			$sql = "SELECT COUNT(*) FROM empleave
+			WHERE empleave.EmpID = '$empID' 
+			AND DATE_FORMAT(empleave.StartDate, '%Y-%m')<='$month' 
+			AND DATE_FORMAT(empleave.EndDate, '%Y-%m')>='$month' AND empleave.LStatus = 'Approved'";
+		}
+		else{
+			$sql = "SELECT COUNT(*) FROM empleave
+			WHERE empleave.EmpID = '$empID' 
+			AND DATE_FORMAT(empleave.StartDate, '%Y-%m')<='$month' 
+			AND DATE_FORMAT(empleave.EndDate, '%Y-%m')>='$month' AND empleave.LStatus = 'Approved'
+			AND empleave.LeaveType = '$type'";
+		}
+
+		return $this->db->runQuery($sql);
 	}
 
 	function getNotifications(){
